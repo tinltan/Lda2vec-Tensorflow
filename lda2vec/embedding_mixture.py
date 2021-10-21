6dbab850ed6346004b92e4a43d7d14506916ba7d
tinltan <rltan3@up.edu.ph>
2021-10-21 20:43:14 +0800

    Update embedding_mixture.py
    Deleted 'name' argument with changes in spacing line 24 25

--- lda2vec/embedding_mixture.py
+++ lda2vec/embedding_mixture.py
@@ -21,8 +21,7 @@ class EmbedMixture:
         self.name = name
         scalar = 1 / np.sqrt(n_documents + n_topics)
         
-        self.doc_embedding = tf.compat.v1.get_variable(tf.random.normal([n_documents, n_topics], mean=0, stddev=50 * scalar),
-                                         name='doc_embedding') if W_in is None else W_in
+        self.doc_embedding = tf.compat.v1.get_variable('doc_embedding', tf.random.normal([n_documents, n_topics], mean=0, stddev=50 * scalar)) if W_in is None else W_in
 
         self.topic_embedding = tf.compat.v1.get_variable('topic_embedding', shape=[n_topics, n_dim],
                                                dtype=tf.float32,
