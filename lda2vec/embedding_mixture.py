39ba0b78888d2d9ad27221d11b68b139af99e9cc
tinltan <rltan3@up.edu.ph>
2021-10-21 18:25:14 +0800

    Update embedding_mixture.py
    Changed to tf.random.normal line 24

--- lda2vec/embedding_mixture.py
+++ lda2vec/embedding_mixture.py
@@ -21,7 +21,7 @@ class EmbedMixture:
         self.name = name
         scalar = 1 / np.sqrt(n_documents + n_topics)
         
-        self.doc_embedding = tf.Variable(tf.random_normal([n_documents, n_topics], mean=0, stddev=50 * scalar),
+        self.doc_embedding = tf.Variable(tf.random.normal([n_documents, n_topics], mean=0, stddev=50 * scalar),
                                          name='doc_embedding') if W_in is None else W_in
 
         self.topic_embedding = tf.get_variable('topic_embedding', shape=[n_topics, n_dim],
